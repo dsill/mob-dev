f5882b41f4a633e0000870587e6ccb10f6d5f337
danielsi <daniel.sill@confirmit.com>
2016-03-23 21:24:10 -0400

    realigned dice on content page and made 2 dice return different different results

--- AlwaysSolved/DrinkingDice/app/src/main/java/com/alwayssolved/drinkingdice/Game_twoDice.java
+++ AlwaysSolved/DrinkingDice/app/src/main/java/com/alwayssolved/drinkingdice/Game_twoDice.java
@@ -37,17 +37,17 @@ public class Game_twoDice extends AppCompatActivity {
         //load dice sound
         sound_id = dice_sound.load(this, R.raw.shake_dice, 1);
 
-        HashMap<Integer, Integer> diceImages = new HashMap();
-        diceImages.put(1, Integer.valueOf(R.drawable.one));
-        diceImages.put(2, Integer.valueOf(R.drawable.two ));
-        diceImages.put(3, Integer.valueOf(R.drawable.three));
-        diceImages.put(4, Integer.valueOf(R.drawable.four ));
-        diceImages.put(5, Integer.valueOf(R.drawable.five));
-        diceImages.put(6, Integer.valueOf(R.drawable.six ));
+/*        HashMap<Integer, Integer> diceImages = new HashMap<>();
+        diceImages.put(1, R.drawable.one);
+        diceImages.put(2, R.drawable.two);
+        diceImages.put(3, R.drawable.three);
+        diceImages.put(4, R.drawable.four);
+        diceImages.put(5, R.drawable.five);
+        diceImages.put(6, R.drawable.six);
 
         for (int i = 1; i < qtyDice; i++) {
             //do something looped
-        }
+        }*/
 
         dicePicture1 = (ImageView) findViewById(R.id.imageView1);
         dicePicture2 = (ImageView) findViewById(R.id.imageView2);
@@ -67,16 +67,14 @@ public class Game_twoDice extends AppCompatActivity {
             //Start rolling sound
             dice_sound.play(sound_id, 1.0f, 1.0f, 0, 0, 1.0f);
             //Pause to allow image to update
-            timer.schedule(new Roll(), 1000);
+            timer.schedule(new Roll(), 600);
         }
     }
 
     //When pause completed message sent to callback
     class Roll extends TimerTask {
         public void run() {
-            for (int i = 1; i < qtyDice; i++) {
-                handler.sendEmptyMessage(0);
-            }
+            handler.sendEmptyMessage(0);
 
         }
     }
@@ -90,26 +88,42 @@ public class Game_twoDice extends AppCompatActivity {
             switch(rng.nextInt(6) + 1) {
                 case 1:
                     dicePicture1.setImageResource(R.drawable.one);
-                    dicePicture2.setImageResource(R.drawable.one);
                     break;
                 case 2:
                     dicePicture1.setImageResource(R.drawable.two);
-                    dicePicture2.setImageResource(R.drawable.two);
                     break;
                 case 3:
                     dicePicture1.setImageResource(R.drawable.three);
-                    dicePicture2.setImageResource(R.drawable.three);
                     break;
                 case 4:
                     dicePicture1.setImageResource(R.drawable.four);
-                    dicePicture2.setImageResource(R.drawable.four);
                     break;
                 case 5:
                     dicePicture1.setImageResource(R.drawable.five);
-                    dicePicture2.setImageResource(R.drawable.five);
                     break;
                 case 6:
                     dicePicture1.setImageResource(R.drawable.six);
+                    break;
+                default:
+            }
+
+            switch(rng.nextInt(6) + 1) {
+                case 1:
+                    dicePicture2.setImageResource(R.drawable.one);
+                    break;
+                case 2:
+                    dicePicture2.setImageResource(R.drawable.two);
+                    break;
+                case 3:
+                    dicePicture2.setImageResource(R.drawable.three);
+                    break;
+                case 4:
+                    dicePicture2.setImageResource(R.drawable.four);
+                    break;
+                case 5:
+                    dicePicture2.setImageResource(R.drawable.five);
+                    break;
+                case 6:
                     dicePicture2.setImageResource(R.drawable.six);
                     break;
                 default:
